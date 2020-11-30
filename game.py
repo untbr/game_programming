55840932c0c1282152c98727a7275729773d892e
kedama <74854260+kedama-nth@users.noreply.github.com>
2020-11-30 19:42:56 +0900

    Add files via upload
    import文周りを変更したメインプログラムと自作モジュール
    colors.pyは定数から列挙型に変更
    それ以外は大体前と同じ

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import sys
 import pygame
 from pygame.locals import *  # 定数読み込み
-from user_interface.align import Align  # オブジェクトの位置を設定するクラ
-from user_interface import colors  # 色の定数と補色を求める関数
+from materials import colors  # 色に関するモジュール
+from materials import align  # オブジェクトの配置に関するモジュール
 
 
 def main():
@@ -18,11 +18,11 @@ def main():
     font = pygame.font.Font(None, 60)  # フォント設定(フォント種類, サイズ(px))
     # 色設定(r, g, b)
     while(True):
-        screen.fill(colors.AQUA)  # ウィンドウを塗りつぶす
+        screen.fill(colors.Color.AQUA.color())  # ウィンドウを塗りつぶす
         # 描画する文字を設定(文字列, アンチエイリアスの有無, 色)
-        text_title = font.render('Game Title', True, colors.comp_color(colors.AQUA))
+        text_title = font.render('Game Title', True, colors.comp_color(colors.Color.AQUA.color()))
         # 文字の表示位置を設定
-        aling = Align(text_title, WIDTH, HEIGHT)  # タイトルの表示位置を操作する準備
+        aling = align.Align(text_title, WIDTH, HEIGHT)  # タイトルの表示位置を操作する準備
         screen.blit(text_title, [aling.align_center(), aling.align_middle()])  # 表示
         pygame.display.update()  # ウィンドウ更新(変更結果を表示)
         # イベント処理
---
+++ materials/align.py
@@ -0,0 +1,44 @@
+"""
+オブジェクトの配置に関するモジュール
+Pygameと合わせて使う
+Alignクラスはインスタンス化のときに引数が必要なので注意
+"""
+
+
+class Align():
+    """
+    Surfaceオブジェクトに対して上下左右の配置をサポートするクラス
+    Surface.blit()等でオブジェクトを画面上に描画するときに合わせて使用する
+    """
+    def __init__(self, surface_obj, win_w: int, win_h: int) -> None:
+        """
+        コンストラクタ
+        surface_obj: Surfaceオブジェクト, win_w: ウィンドウの横幅, win_h: ウィンドウの縦幅
+        """
+        self.obj = surface_obj
+        self.win_w = win_w
+        self.win_h = win_h
+
+    def align_left(self) -> int:
+        """オブジェクトを水平方向に対して左寄せする関数"""
+        return 0
+
+    def align_right(self) -> int:
+        """オブジェクトを水平方向に対して右寄せする関数"""
+        return (self.win_w - self.obj.get_width())
+
+    def align_center(self) -> int:
+        """オブジェクトを水平方向に対して中央寄せする関数"""
+        return int((self.win_w/2) - (self.obj.get_width()/2))
+
+    def align_top(self) -> int:
+        """オブジェクトを垂直方向に対して上寄せする関数"""
+        return 0
+
+    def align_bottom(self) -> int:
+        """オブジェクトを垂直方向に対して下寄せする関数"""
+        return (self.win_h - self.obj.get_height())
+
+    def align_middle(self) -> int:
+        """オブジェクトを垂直方向に対して中央寄せする関数"""
+        return int((self.win_h/2) - (self.obj.get_height()/2))

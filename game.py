3e1dc86bcc71e6a31e74654d7e4cbb63d4c54176
DYGV <11eisuke88@gmail.com>
2020-12-04 01:57:34 +0900

    修正: 最新コミットでの動作サポート

--- game.py
+++ game.py
@@ -2,8 +2,8 @@ import sys
 import textwrap
 import pygame
 from pygame.locals import *  # 定数読み込み
-from materials import colors  # 色に関するモジュール
-from materials import align  # オブジェクトの配置に関するモジュール
+from materials.colors import Color  # 色に関するモジュール
+from materials.align import Align  # オブジェクトの配置に関するモジュール
 from materials import events  # イベント処理に関するモジュール
 
 
@@ -22,15 +22,15 @@ def main():
     font_text = pygame.font.SysFont('yumincho', 30)  # 本文
     # タイトル画面 --------------------------------------------------------------------------------
     is_running = True
-    screen.fill(colors.Color.BLACK.color())  # ウィンドウを塗りつぶす
+    screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
     # 描画する文字を設定(文字列, アンチエイリアスの有無, 色)
-    text_h = font_title.render('タイピングゲーム(仮)', True, colors.Color.WHITE.color())
-    text_p = font_text.render('Please press any key...', True, colors.Color.WHITE.color())
+    text_h = font_title.render('タイピングゲーム(仮)', True, Color.WHITE.rgb)
+    text_p = font_text.render('Please press any key...', True, Color.WHITE.rgb)
     # 文字の表示位置を設定
-    aling = align.Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_h, [aling.align_center(), aling.align_middle() - text_h.get_height()*2])
-    aling = align.Align(text_p, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_p, [aling.align_center(), aling.align_middle()])
+    align = Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
+    align = Align(text_p, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_p, [align.center(), align.middle()])
     pygame.display.update()  # ウィンドウ更新(変更結果を表示)
     while(is_running):
         # イベント処理
@@ -41,16 +41,16 @@ def main():
     is_running = True
     level = ''
     # 難易度選択 --------------------------------------------------------------------------------
-    screen.fill(colors.Color.BLACK.color())  # ウィンドウを塗りつぶす
-    text_h = font_title.render('難易度選択', True, colors.Color.WHITE.color())
-    text_p1 = font_text.render('やさしい: f', True, colors.Color.WHITE.color())
-    text_p2 = font_text.render('むずかしい: j', True, colors.Color.WHITE.color())
-    aling = align.Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_h, [aling.align_center(), aling.align_middle() - text_h.get_height()*2])
-    aling = align.Align(text_p1, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_p1, [aling.align_center(), aling.align_middle()])
-    aling = align.Align(text_p2, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_p2, [aling.align_center(), aling.align_middle() + text_p2.get_height()])
+    screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
+    text_h = font_title.render('難易度選択', True, Color.WHITE.rgb)
+    text_p1 = font_text.render('やさしい: f', True, Color.WHITE.rgb)
+    text_p2 = font_text.render('むずかしい: j', True, Color.WHITE.rgb)
+    align = Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
+    align = Align(text_p1, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_p1, [align.center(), align.middle()])
+    align = Align(text_p2, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_p2, [align.center(), align.middle() + text_p2.get_height()])
     pygame.display.update()  # ウィンドウ更新(変更結果を表示)
     while(is_running):
         # イベント処理
@@ -66,22 +66,22 @@ def main():
     # プレイ画面 --------------------------------------------------------------------------------
     is_running = True
     # ここの辺でデータセットからの読み込みが発生する(？)
-    screen.fill(colors.Color.BLACK.color())  # ウィンドウを塗りつぶす
-    text_h = font_text.render('時間内に入力せよ！', True, colors.Color.WHITE.color())
-    text_time = font_text.render('00:00', True, colors.Color.WHITE.color())
-    text_word = font_text.render('オブ○ェクト', True, colors.Color.WHITE.color())
-    # text_desc = font_text.render('オブジェクトとは、物、物体、目標物、対象、目的語、客体、などの意味を持つ英単語。', True, colors.Color.WHITE.color())  # 18文字ごとで区切った配列を作成→forで出力
-    aling = align.Align(text_h, WIDTH, HEIGHT)
-    screen.blit(text_h, [aling.align_left() + 10, aling.align_top() + 10])
-    aling = align.Align(text_time, WIDTH, HEIGHT)
-    screen.blit(text_time, [aling.align_right() - 10, aling.align_top() + 10])
-    aling = align.Align(text_word, WIDTH, HEIGHT)
-    screen.blit(text_word, [aling.align_center(), aling.align_middle() - text_h.get_height()*3])
+    screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
+    text_h = font_text.render('時間内に入力せよ！', True, Color.WHITE.rgb)
+    text_time = font_text.render('00:00', True, Color.WHITE.rgb)
+    text_word = font_text.render('オブ○ェクト', True, Color.WHITE.rgb)
+    # text_desc = font_text.render('オブジェクトとは、物、物体、目標物、対象、目的語、客体、などの意味を持つ英単語。', True, Color.WHITE.rgb)  # 18文字ごとで区切った配列を作成→forで出力
+    align = Align(text_h, WIDTH, HEIGHT)
+    screen.blit(text_h, [align.left() + 10, align.top() + 10])
+    align = Align(text_time, WIDTH, HEIGHT)
+    screen.blit(text_time, [align.right() - 10, align.top() + 10])
+    align = Align(text_word, WIDTH, HEIGHT)
+    screen.blit(text_word, [align.center(), align.middle() - text_h.get_height()*3])
     text = 'オブジェクトとは、物、物体、目標物、対象、目的語、客体、などの意味を持つ英単語。'
     for i, desc in enumerate(textwrap.wrap(text, 18)):
-        text_desc = font_text.render(desc, True, colors.Color.WHITE.color())
-        aling = align.Align(text_desc, WIDTH, HEIGHT)
-        screen.blit(text_desc, [aling.align_center(), aling.align_middle() + text_desc.get_height()*i - text_desc.get_height()])
+        text_desc = font_text.render(desc, True, Color.WHITE.rgb)
+        align = Align(text_desc, WIDTH, HEIGHT)
+        screen.blit(text_desc, [align.center(), align.middle() + text_desc.get_height()*i - text_desc.get_height()])
     pygame.display.update()
     while(is_running):
         # イベント処理
@@ -91,19 +91,19 @@ def main():
                 is_running = False
     # リザルト画面 --------------------------------------------------------------------------------
     is_running = True
-    screen.fill(colors.Color.BLACK.color())  # ウィンドウを塗りつぶす
-    text_h = font_title.render('リザルト', True, colors.Color.WHITE.color())
-    aling = align.Align(text_h, WIDTH, HEIGHT)
-    screen.blit(text_h, [aling.align_center(), aling.align_middle() - text_h.get_height()*2])
-    text_p1 = font_text.render('正解：{0}'.format(0), True, colors.Color.WHITE.color())
-    aling = align.Align(text_p1, WIDTH, HEIGHT)
-    screen.blit(text_p1, [aling.align_center(), aling.align_middle()])
-    text_p2 = font_text.render('不正解：{0}'.format(0), True, colors.Color.WHITE.color())
-    aling = align.Align(text_p2, WIDTH, HEIGHT)
-    screen.blit(text_p2, [aling.align_center(), aling.align_middle() + text_p2.get_height()])
-    text_p3 = font_text.render('Please press any key...'.format(0), True, colors.Color.WHITE.color())
-    aling = align.Align(text_p3, WIDTH, HEIGHT)
-    screen.blit(text_p3, [aling.align_center(), aling.align_middle() + text_p3.get_height()*3])
+    screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
+    text_h = font_title.render('リザルト', True, Color.WHITE.rgb)
+    align = Align(text_h, WIDTH, HEIGHT)
+    screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
+    text_p1 = font_text.render('正解：{0}'.format(0), True, Color.WHITE.rgb)
+    align = Align(text_p1, WIDTH, HEIGHT)
+    screen.blit(text_p1, [align.center(), align.middle()])
+    text_p2 = font_text.render('不正解：{0}'.format(0), True, Color.WHITE.rgb)
+    align = Align(text_p2, WIDTH, HEIGHT)
+    screen.blit(text_p2, [align.center(), align.middle() + text_p2.get_height()])
+    text_p3 = font_text.render('Please press any key...'.format(0), True, Color.WHITE.rgb)
+    align = Align(text_p3, WIDTH, HEIGHT)
+    screen.blit(text_p3, [align.center(), align.middle() + text_p3.get_height()*3])
     pygame.display.update()
     while(is_running):
         # イベント処理
@@ -113,15 +113,15 @@ def main():
                 is_running = False
     # タイトル画面 --------------------------------------------------------------------------------
     is_running = True
-    screen.fill(colors.Color.BLACK.color())  # ウィンドウを塗りつぶす
+    screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
     # 描画する文字を設定(文字列, アンチエイリアスの有無, 色)
-    text_h = font_title.render('タイピングゲーム(仮)', True, colors.Color.WHITE.color())
-    text_p = font_text.render('Please press any key...', True, colors.Color.WHITE.color())
+    text_h = font_title.render('タイピングゲーム(仮)', True, Color.WHITE.rgb)
+    text_p = font_text.render('Please press any key...', True, Color.WHITE.rgb)
     # 文字の表示位置を設定
-    aling = align.Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_h, [aling.align_center(), aling.align_middle() - text_h.get_height()*2])
-    aling = align.Align(text_p, WIDTH, HEIGHT)  # 表示位置を操作する準備
-    screen.blit(text_p, [aling.align_center(), aling.align_middle()])
+    align = Align(text_h, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
+    align = Align(text_p, WIDTH, HEIGHT)  # 表示位置を操作する準備
+    screen.blit(text_p, [align.center(), align.middle()])
     pygame.display.update()  # ウィンドウ更新(変更結果を表示)
     while(is_running):
         # イベント処理

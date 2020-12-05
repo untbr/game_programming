7b9210a15c132d222a3ac2527a26dd7184756633
DYGV <dygv1188@gmail.com>
2020-12-06 00:30:47 +0900

    変更: 状態管理の仕方

--- materials/state.py
+++ materials/state.py
@@ -6,132 +6,166 @@
 
 import textwrap
 import pygame
+from enum import Enum
 from pygame.locals import *  # 定数読み込み
 from .colors import Color  # 色に関するモジュール
 from .align import Align  # オブジェクトの配置に関するモジュール
 from . import events  # イベント処理に関するモジュール
 
 
-class State:
-    """
-    各画面を状態として捉えて処理を行うクラス
-    インスタンス作成後、title→choose→play→resultメソッドを順次呼び出していく
-    """
+class States(Enum):
+    """状態の定義"""
+    TITLE = 0
+    CHOOSE = 1
+    PLAY = 2
+    RESULT = 3
 
+
+class State:
+    def __init__(self):
+        self.state = None
+
+    def transition(self):
+        """現在時の状態とキーの押下に応じて状態を更新するメソッド"""
+        for event in pygame.event.get():
+            if self.state is None: # 初期状態のNoneであれば
+                self.state = States.TITLE
+            # 状態がTITLEでキーの押下があったとき
+            elif self.state == States.TITLE and event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.state = States.CHOOSE  # 次の状態CHOOSEに遷移する
+            # 状態がCHOOSEでキーの押下があったとき
+            elif self.state == States.CHOOSE and event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.state = States.PLAY  # 次の状態PLAYに遷移する
+            # 状態がPLAYでキーの押下があったとき
+            elif self.state == States.PLAY and event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.state = States.RESULT  # 次の状態RESULTに遷移する
+            # 状態がRESULTでキーの押下があったとき
+            elif self.state == States.RESULT and event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    self.state = States.TITLE  # 次の状態TITLEにする
+            if event.type == QUIT:
+                events.quit_game()  # 閉じるボタン押下で終了
+
+
+"""
+各画面を状態として捉えて処理を行うクラス
+インスタンス作成後、title→choose→play→resultメソッドを順次呼び出していく
+"""
+
+
+class Drawer(State):
     def __init__(self, width: int, height: int) -> None:
         """
         コンストラクタ
         width: ウィンドウの横幅, height: ウィンドウの縦幅
         """
+        super().__init__()
         self.width = width  # ウィンドウ横幅
         self.height = height  # ウィンドウ縦幅
-        self.is_running = True  # ゲームループの判定
         pygame.init()  # Pygame初期化
-        self.screen = pygame.display.set_mode((self.width, self.height))  # ウィンドウ生成(横, 縦)
-        self.font_S = pygame.font.SysFont('yumincho', 15)  # テキスト：小
-        self.font_M = pygame.font.SysFont('yumincho', 30)  # テキスト：中
-        self.font_L = pygame.font.SysFont('yumincho', 60)  # テキスト：大
+        self.screen = pygame.display.set_mode(
+            (self.width, self.height)
+        )  # ウィンドウ生成(横, 縦)
+        self.font_S = pygame.font.SysFont("yumincho", 15)  # テキスト：小
+        self.font_M = pygame.font.SysFont("yumincho", 30)  # テキスト：中
+        self.font_L = pygame.font.SysFont("yumincho", 60)  # テキスト：大
 
     def title(self) -> None:
         """タイトル画面"""
-        pygame.display.set_caption('タイピングゲーム(仮) | Title')  # キャプション設定
+        pygame.display.set_caption("タイピングゲーム(仮) | Title")  # キャプション設定
         self.screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
         # 画面に表示するテキストの設定
-        text_h = self.font_L.render('タイピングゲーム(仮)', True, Color.WHITE.rgb)  # タイトル
+        text_h = self.font_L.render("タイピングゲーム(仮)", True, Color.WHITE.rgb)  # タイトル
         align = Align(text_h, self.width, self.height)
-        self.screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
-        text_p = self.font_M.render('Please press any key...', True, Color.WHITE.rgb)  # 操作の促し
+        self.screen.blit(
+            text_h, [align.center(), align.middle() - text_h.get_height() * 2]
+        )
+        text_p = self.font_M.render(
+            "Please press any key...", True, Color.WHITE.rgb
+        )  # 操作の促し
         align = Align(text_p, self.width, self.height)
         self.screen.blit(text_p, [align.center(), align.middle()])
         pygame.display.update()  # 画面更新
-        self.is_running = True
-        while(self.is_running):
-            # イベント処理
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    events.quit_game()  # 閉じるボタン押下で終了
-                if event.type == KEYDOWN:
-                    self.is_running = False  #キー入力検知で次の画面へ
 
     def choose(self) -> None:
         """難易度選択画面"""
-        pygame.display.set_caption('タイピングゲーム(仮) | Choose')  # キャプション設定
+        pygame.display.set_caption("タイピングゲーム(仮) | Choose")  # キャプション設定
         self.screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
         # 画面に表示するテキストの設定
-        text_h = self.font_L.render('難易度選択', True, Color.WHITE.rgb)  # タイトル
+        text_h = self.font_L.render("難易度選択", True, Color.WHITE.rgb)  # タイトル
         align = Align(text_h, self.width, self.height)
-        self.screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
+        self.screen.blit(
+            text_h, [align.center(), align.middle() - text_h.get_height() * 2]
+        )
         text_p1 = self.font_M.render('やさしい: Input " f "', True, Color.WHITE.rgb)  # 難易度1
         align = Align(text_p1, self.width, self.height)
         self.screen.blit(text_p1, [align.center(), align.middle()])
-        text_p2 = self.font_M.render('むずかしい: Input " j "', True, Color.WHITE.rgb)  # 難易度2
+        text_p2 = self.font_M.render(
+            'むずかしい: Input " j "', True, Color.WHITE.rgb
+        )  # 難易度2
         align = Align(text_p2, self.width, self.height)
-        self.screen.blit(text_p2, [align.center(), align.middle() + text_p2.get_height()])
+        self.screen.blit(
+            text_p2, [align.center(), align.middle() + text_p2.get_height()]
+        )
         pygame.display.update()  # 画面更新
-        self.is_running = True
-        while(self.is_running):
-            # イベント処理
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    events.quit_game()  # 閉じるボタン押下で終了
-                if event.type == KEYDOWN:
-                    if events.input_catch(event.key) == 'f':  # 「やさしい」を選択
-                        self.is_running = False
-                    elif events.input_catch(event.key) == 'j':  # 「むずかしい」を選択
-                        self.is_running = False
 
     def play(self) -> None:
         """ゲームプレイ画面"""
-        pygame.display.set_caption('タイピングゲーム(仮) | Play')  # キャプション設定
+        pygame.display.set_caption("タイピングゲーム(仮) | Play")  # キャプション設定
         self.screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
         # 画面に表示するテキストの設定
-        text_h = self.font_M.render('時間内に入力せよ！', True, Color.WHITE.rgb)  # 小見出し
+        text_h = self.font_M.render("時間内に入力せよ！", True, Color.WHITE.rgb)  # 小見出し
         align = Align(text_h, self.width, self.height)
         self.screen.blit(text_h, [align.left() + 10, align.top() + 10])
-        text_time = self.font_M.render('00:00', True, Color.WHITE.rgb)  # 残り時間
+        text_time = self.font_M.render("00:00", True, Color.WHITE.rgb)  # 残り時間
         align = Align(text_time, self.width, self.height)
         self.screen.blit(text_time, [align.right() - 10, align.top() + 10])
-        text_word = self.font_L.render('オブ○ェクト', True, Color.WHITE.rgb)  # 単語
+        text_word = self.font_L.render("オブ○ェクト", True, Color.WHITE.rgb)  # 単語
         align = Align(text_word, self.width, self.height)
-        self.screen.blit(text_word, [align.center(), align.middle() - text_h.get_height()*3])
-        text = 'オブジェクトとは、物、物体、目標物、対象、目的語、客体、などの意味を持つ英単語。'
+        self.screen.blit(
+            text_word, [align.center(), align.middle() - text_h.get_height() * 3]
+        )
+        text = "オブジェクトとは、物、物体、目標物、対象、目的語、客体、などの意味を持つ英単語。"
         for i, desc in enumerate(textwrap.wrap(text, 18)):
-            text_desc = self.font_M.render(desc, True, Color.WHITE.rgb)  #説明
+            text_desc = self.font_M.render(desc, True, Color.WHITE.rgb)  # 説明
             align = Align(text_desc, self.width, self.height)
-            self.screen.blit(text_desc, [align.center(), align.middle() + text_desc.get_height()*i - text_desc.get_height()])
+            self.screen.blit(
+                text_desc,
+                [
+                    align.center(),
+                    align.middle()
+                    + text_desc.get_height() * i
+                    - text_desc.get_height(),
+                ],
+            )
         pygame.display.update()  # 画面更新
-        self.is_running = True
-        while(self.is_running):
-            # イベント処理
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    events.quit_game()  # 閉じるボタン押下で終了
-                if event.type == KEYDOWN:
-                    self.is_running = False  #キー入力検知で次の画面へ
 
     def result(self):
         """リザルト画面"""
-        pygame.display.set_caption('タイピングゲーム(仮) | Result')  # キャプション設定
+        pygame.display.set_caption("タイピングゲーム(仮) | Result")  # キャプション設定
         self.screen.fill(Color.BLACK.rgb)  # ウィンドウを塗りつぶす
         # 画面に表示するテキストの設定
-        text_h = self.font_L.render('リザルト', True, Color.WHITE.rgb)  # ヘッダー
+        text_h = self.font_L.render("リザルト", True, Color.WHITE.rgb)  # ヘッダー
         align = Align(text_h, self.width, self.height)
-        self.screen.blit(text_h, [align.center(), align.middle() - text_h.get_height()*2])
-        text_p1 = self.font_M.render('正解：{0}'.format(0), True, Color.WHITE.rgb)  # 正解
+        self.screen.blit(
+            text_h, [align.center(), align.middle() - text_h.get_height() * 2]
+        )
+        text_p1 = self.font_M.render("正解：{0}".format(0), True, Color.WHITE.rgb)  # 正解
         align = Align(text_p1, self.width, self.height)
         self.screen.blit(text_p1, [align.center(), align.middle()])
-        text_p2 = self.font_M.render('不正解：{0}'.format(0), True, Color.WHITE.rgb)  # 不正解
+        text_p2 = self.font_M.render("不正解：{0}".format(0), True, Color.WHITE.rgb)  # 不正解
         align = Align(text_p2, self.width, self.height)
-        self.screen.blit(text_p2, [align.center(), align.middle() + text_p2.get_height()])
-        text_p3 = self.font_M.render('Please press any key...'.format(0), True, Color.WHITE.rgb)  # 操作の促し
+        self.screen.blit(
+            text_p2, [align.center(), align.middle() + text_p2.get_height()]
+        )
+        text_p3 = self.font_M.render(
+            "Please press any key...".format(0), True, Color.WHITE.rgb
+        )  # 操作の促し
         align = Align(text_p3, self.width, self.height)
-        self.screen.blit(text_p3, [align.center(), align.middle() + text_p3.get_height()*3])
+        self.screen.blit(
+            text_p3, [align.center(), align.middle() + text_p3.get_height() * 3]
+        )
         pygame.display.update()  # 画面更新
-        self.is_running = True
-        while(self.is_running):
-            # イベント処理
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    events.quit_game()  # 閉じるボタン押下で終了
-                if event.type == KEYDOWN:
-                    self.is_running = False  #キー入力検知で次の画面へ
--- test_game.py
+++ test_game.py
@@ -5,7 +5,7 @@ from pygame.locals import *  # 定数読み込み
 from materials.colors import Color  # 色に関するモジュール
 from materials.align import Align  # オブジェクトの配置に関するモジュール
 from materials import events  # イベント処理に関するモジュール
-from materials.state import State  # 状態
+from materials.state import Drawer, States  # 状態
 
 
 def main():
@@ -13,14 +13,24 @@ def main():
     ゲームのサンプル
     ゲームはキーボードを適当に操作することにより進行、2回目のタイトル画面表示後終了する
     """
-    game = State(800, 600)  # インスタンス作成
-    game.title()  # タイトル画面
-    game.choose()  # 難易度選択
-    game.play()  # ゲームプレイ画面
-    game.result()  # リザルト画面
-    game.title()  # タイトル画面
+    game = Drawer(800, 600)  # インスタンス作成
+    while True:
+        current_state = game.state
+        game.transition()
+        # 状態遷移がされてなければ(同じ状態であるなら)、ループの頭に戻る
+        if game.state == current_state:
+            continue
+        # 状態遷移がされていれば、描画を更新する
+        if game.state == States.TITLE:
+            game.title()
+        if game.state == States.CHOOSE:
+            game.choose()
+        if game.state == States.PLAY:
+            game.play()
+        if game.state == States.RESULT:
+            game.result()
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

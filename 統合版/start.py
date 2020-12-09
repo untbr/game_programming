c33948f6a649d2713c473e50c67cfa54196887a9
DYGV <11eisuke88@gmail.com>
2020-12-10 07:00:37 +0900

    削除: 不要なimport

--- 統合版/start.py
+++ 統合版/start.py
@@ -1,10 +1,3 @@
-#import sys
-#import textwrap
-#import pygame
-#from pygame.locals import *  # 定数読み込み
-#from materials.colors import Color  # 色に関するモジュール
-#from materials.align import Align  # オブジェクトの配置に関するモジュール
-#from materials import events  # イベント処理に関するモジュール
 from materials.state import State  # 状態
 
 

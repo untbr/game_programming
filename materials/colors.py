4da992159c4b25f879c3a02cff5e2334cad9446e
DYGV <11eisuke88@gmail.com>
2021-01-07 01:24:13 +0900

    使わないカラーコードの削除

--- materials/colors.py
+++ materials/colors.py
@@ -8,23 +8,11 @@ class Color(Enum):
     名前はname, 値はvalueで取得可能
     """
 
-    WHITE = (255, 255, 255)  # color code: ffffff
-    SILVER = (192, 192, 192)  # color code: c0c0c0
-    GRAY = (128, 128, 128)  # color code: 808080
     BLACK = (0, 0, 0)  # color code: 000000
     RED = (255, 0, 0)  # color code: ff0000
-    MAROON = (128, 0, 0)  # color code: 800000
-    YELLOW = (255, 255, 0)  # color code: ffff00
-    OLIVE = (128, 128, 0)  # color code: 808000
-    LIME = (0, 255, 0)  # color code: 00ff00
-    GREEN = (0, 128, 0)  # color code: 008000
-    AQUA = (0, 255, 255)  # color code: 00ffff
     TEAL = (0, 128, 128)  # color code: 008080
     BLUE = (0, 0, 255)  # color code: 0000ff
-    NAVY = (0, 0, 128)  # color code: 000080
-    FUCHSIA = (255, 0, 255)  # color code: ff00ff
-    PURPLE = (128, 0, 128)  # color code: 800080
-    WAKATAKE = (112, 225, 112)
+    WAKATAKE = (112, 225, 112) # color code: 70e170
 
     @property
     def rgb(self) -> tuple:
